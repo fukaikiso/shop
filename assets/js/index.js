db6e4ca08d35ea105dfab80c8905241597091991
fukaikiso <fukaikiso@qq.com>
2022-06-15 16:15:00 +0800

    header-respose

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,22 @@
     // 填充文字
     const d = document.querySelector('.page-wrapper');
     for (let i = 0; i < 100; i++) {
-        $(`<div>hhhhhhhhhhhhhhhhhh</div>`).appendTo(d)
+        $(`<div>${i}+hhhhhhhhhhhhhhhhhh</div>`).appendTo(d)
     }
 
     // header fix后脱离文档流，需要重新占据高度占据高度
-    $('.header-placeholder').css({
-        'height': `${$('.main-header').height()}`,
+    $('.main-header').next().css({
+        'margin-top': `${$('.main-header').height()}px`,
     })
-    // 检测屏幕宽度，若小于md，则改变排版
+
+
+    //menu-collections悬浮效果
+    $('.menu-collections>span').eq(0).mouseover(function () {
+        $(this).toggleClass('bi-heart')
+        $(this).toggleClass('bi-heart-fill')
+        $(this)
+    })
+
 
 
 })(window.jQuery);
