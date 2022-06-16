6df355b234d9db7bf79ff930e4fa4e7d70f6fdf9
fukaikiso <fukaikiso@qq.com>
2022-06-16 23:53:34 +0800

    add banner timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,35 @@
 
     "use strict";
 
+    //轮播图圆点
+    $('.banner .dots>span').mouseover(function () {
+        $(this).addClass('active').siblings().removeClass('active')
+        const index = $(this).index()
+        $('.banner .imgs>img').eq(0).stop().animate({ marginLeft: `-${index}00%` })
+    })
+    //轮播图按钮
+    $('.banner .btns>img').eq(0).click(function () {
+        const $prev = $('.banner .active').prev()
+        if ($prev.length) {
+            $prev.mouseover()
+        } else {
+            $('.banner .dots>span').eq(-1).mouseover()
+        }
+    })
 
+    $('.banner .btns>img').eq(1).click(function () {
+        const $next = $('.banner .active').next()
+        if ($next.length) {
+            $next.mouseover()
+        } else {
+            $('.banner .dots>span').eq(0).mouseover()
+        }
+    })
+
+    //定时器
+    setInterval(() => {
+        $('.banner .btns>img').eq(1).click()
+    }, 2000);
 
 
 })(window.jQuery);
