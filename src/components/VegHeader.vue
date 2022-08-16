4534ad650411028e6939f56e948f1b78bb488cd3
fukaikiso <fukaikiso@qq.com>
2022-08-17 00:12:18 +0800

    update header

--- src/components/VegHeader.vue
+++ src/components/VegHeader.vue
@@ -1,5 +1,152 @@
 <template>
   <div>
+    <a class="position-absolute" href="javascript:void(0)" onclick="cartopen()">
+        <div id="sitebar-drawar" class="sitebar-drawar">
+            <div class="cart-count d-flex align-items-center">
+                <i class="fas fa-shopping-basket"></i>
+                <span>3 Item</span>
+            </div>
+            <div class="total-price">$15.00</div>
+        </div>
+    </a>
+
+     <!-- admin Modal -->
+     <div class="modal fade" id="useradmin1" tabindex="-1" aria-labelledby="useradmin1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="header-top-action-dropdown">
+                        <ul>
+                            <li class="signin-option"><a onclick="OpenSignUpForm()" href="#" data-dismiss="modal"><i class="fas fa-user mr-2"></i>Sign In</a></li>
+                            <li class="site-phone"><a href=""><i class="fas fa-phone"></i> 000 000 000</a></li>
+                            <li class="site-help"><a href="#"><i class="fas fa-question-circle"></i> Help & More</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+     <!--siteinfo Modal -->
+     <div class="modal fade" id="siteinfo1" tabindex="-1" aria-labelledby="siteinfo1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="header-top-action-dropdown">
+                        <ul>
+                            <li class="site-phone"><a href=""><i class="fas fa-phone"></i> 000 000 000</a></li>
+                            <li class="site-help"><a href="#"><i class="fas fa-question-circle"></i> Help & More</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+     <!--search Modal -->
+     <div class="modal fade" id="search-select-id" tabindex="-1" aria-labelledby="search-select-id" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="select-search-option">
+                        <div class="flux-custom-select">
+                            <select>
+                              <option value="0">Select Catagory</option>
+                              <option value="1">Vegetables</option>
+                              <option value="2">Fruits</option>
+                              <option value="3">Salads</option>
+                              <option value="4">Fish & Seafood</option>
+                              <option value="5">Fresh Meat</option>
+                              <option value="6">Health Product</option>
+                              <option value="7">Butter & Eggs</option>
+                              <option value="8">Oils & Venegar</option>
+                              <option value="9">Frozen Food</option>
+                              <option value="10">Jam & Honey</option>
+                            </select>
+                        </div>
+                        <form action="#" class="search-form">
+                            <input type="text" name="search" placeholder="Search for Products">
+                            <button class="submit-btn"><i class="fas fa-search"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+     <!-- menu modal -->
+     <div class="modal fade" id="menu-id" tabindex="-1" aria-labelledby="menu-id" aria-hidden="true">
+        <div class="modal-dialog  modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <ul class="menu d-xl-flex flex-wrap pl-0 list-unstyled">
+                        <li class="item-has-children"><a class="active" data-toggle="collapse" href="#mainmenuid1" role="button" aria-expanded="false" aria-controls="catagory-widget1"><span>Home</span> <i class="fas fa-angle-down"></i></a>
+                            <ul class="submenu collapse" id="mainmenuid1">
+                                <li><a href="home-default.html">Home Default</a></li>
+                                <li><a class="active" href="index.html">Home Sticky Sidebar</a></li>
+                                <li><a href="home-search.html">Home Search</a></li>
+                                <li><a href="home-slider.html">Home Slider</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">New Products</a></li>
+                        <li><a data-toggle="collapse" href="#megamenu-main" role="button" aria-expanded="false" aria-controls="catagory-widget1"><span>Featured Products</span> <i class="fas fa-angle-down"></i></a>
+                            <ul class=" collapse" id="megamenu-main">
+                                <li><a data-toggle="collapse" href="#megamenu-main01" role="button" aria-expanded="false" aria-controls="megamenu-main01"><span>Vegetables</span> <i class="fas fa-angle-down"></i></a>
+                                    <ul class="submenu collapse" id="megamenu-main01">
+                                        <li><a href="product-list.html">Artichoke.</a></li>
+                                        <li><a href="product-list.html">Aubergune(eggpiant)</a></li>
+                                        <li><a href="product-list.html">Asparagus</a></li>
+                                        <li><a href="product-list.html">Broccoflower(a hyrbid)</a></li>
+                                    </ul>
+                                </li>
+                                <li><a data-toggle="collapse" href="#megamenu-main02" role="button" aria-expanded="false" aria-controls="megamenu-main02"><span>Fruits</span> <i class="fas fa-angle-down"></i></a>
+                                    <ul class="submenu collapse" id="megamenu-main02">
+                                        <li><a href="product-list.html">Artichoke.</a></li>
+                                        <li><a href="product-list.html">Aubergune(eggpiant)</a></li>
+                                        <li><a href="product-list.html">Asparagus</a></li>
+                                        <li><a href="product-list.html">Broccoflower(a hyrbid)</a></li>
+                                    </ul>
+                                </li>
+                                <li><a data-toggle="collapse" href="#megamenu-main03" role="button" aria-expanded="false" aria-controls="megamenu-main03"><span>Salads</span> <i class="fas fa-angle-down"></i></a>
+                                    <ul class="submenu collapse" id="megamenu-main03">
+                                        <li><a href="product-list.html">Artichoke.</a></li>
+                                        <li><a href="product-list.html">Aubergune(eggpiant)</a></li>
+                                        <li><a href="product-list.html">Asparagus</a></li>
+                                        <li><a href="product-list.html">Broccoflower(a hyrbid)</a></li>
+                                    </ul>
+                                </li>
+                                <li><a data-toggle="collapse" href="#megamenu-main04" role="button" aria-expanded="false" aria-controls="megamenu-main04"><span>Health Care</span> <i class="fas fa-angle-down"></i></a>
+                                    <ul class="submenu collapse" id="megamenu-main04">
+                                        <li><a href="product-list.html">Artichoke.</a></li>
+                                        <li><a href="product-list.html">Aubergune(eggpiant)</a></li>
+                                        <li><a href="product-list.html">Asparagus</a></li>
+                                        <li><a href="product-list.html">Broccoflower(a hyrbid)</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="item-has-children"><a data-toggle="collapse" href="#mainmenuid2" role="button" aria-expanded="false" aria-controls="catagory-widget1"><span>Pages</span> <i class="fas fa-angle-down"></i></a>
+                            <ul class="submenu collapse" id="mainmenuid2">
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="product-list.html">Product List</a></li>
+                                <li><a href="product-leftsidebar.html">Product leftsidebar</a></li>
+                                <li><a href="product-fullwidth.html">Product Fullwidth</a></li>
+                                <li><a href="product-detail.html">Product Details</a></li>
+                                <li><a href="checkout.html">Checkout</a></li>
+                                <li><a href="user-dashbord.html">User Dashboard</a></li>
+                                <li><a href="profile.html">Profile</a></li>
+                                <li><a href="track-order.html">Track Order</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <header class="header">
         <div class="header-top">
             <div class="mobile-header d-flex justify-content-between align-items-center d-xl-none">
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="sticky-sidebar-home">
     <veg-header></veg-header>
   </div>
 </template>
